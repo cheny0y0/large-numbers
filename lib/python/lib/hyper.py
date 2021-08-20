081e842586453fe21a68070a2dfef605b5922050
cheny0y0 <66211043+cheny0y0@users.noreply.github.com>
2021-08-20 06:29:04 +0000

    Create hyper.py

---
+++ lib/python/lib/hyper.py
@@ -0,0 +1,21 @@
+def hyper(m, n, r) :
+    """Do the hyper level calculation.
+hyper(m, 1, r) -> m + r
+hyper(m, 2, r) -> m * r
+hyper(m, 3, r) -> m ** r"""
+    
+    if not isinstance(n, int) :
+        raise TypeError("argument 2 must be int, not "+str(type(n)).split("'")[1])
+    if n <= 0 :
+        raise ValueError("argument 2 must be a positive")
+    if n == 1 :
+        return m + r
+    if n == 2 :
+        return m * r
+    if n == 3 :
+        return m ** r
+    else :
+        res = 1
+        for i in range(r) :
+            res = hyper(m, n-1, res)
+        return res
